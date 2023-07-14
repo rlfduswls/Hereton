f87994d55df638ca8f5ec26925f095677bca0608
ose0221 <ose0221@naver.com>
2023-07-14 23:27:08 +0900

    css 디테일 수정

---
+++ js/mypage.js
@@ -0,0 +1,14 @@
+document.querySelector("#sendButton").addEventListener("click", () => {
+    let selectFile = document.querySelector("#inputImage").files[0];
+    console.log(selectFile);
+});
+document.querySelector("#sendButton").addEventListener("click", () => {
+    let selectFile = document.querySelector("#inputImage").files[0];
+    const file = URL.createObjectURL(selectFile);
+    document.querySelector(".uploadImage").src = file;
+});
+
+$("#file").on("change", function () {
+    var fileName = $("#file").val();
+    $(".upload-name").val(fileName);
+});
